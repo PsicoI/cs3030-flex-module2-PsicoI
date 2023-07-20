f5ccacd6fafff33bb7cdbca9a8dae5cafef88125
Hugo Valle <hugo.valle@gmail.com>
2023-07-20 16:31:34 -0600

    Include la and ca starting code

---
+++ ca/ca.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# timestampt flags
+ts=`date +%Y-%m-%d`
+year=`date +%Y`
+month=`date +%m`
+day=`date +%d`
+log_folder="log/${year}/${month}/${day}"
+
+help()
+{
+  # help function here
+}
+
+check_log_folder()
+{
+  # check log folder
+}
+
+
+
+echo "Done"
+
+
+exit 0
