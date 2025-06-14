44ac6bd4a56b6636402ea5d4d1aae30cb99532a8
PsicoI <cody.turek@hotmail.com>
2025-06-13 21:22:56 -0600

    Created changes to file_eval.sh to allow for custom dates relevant to the day files to be created

--- la/file_eval.sh
+++ la/file_eval.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
 # Represents the file that you want to test. 
-FILE="sales.csv"
+temp="sales_"
+ext=".csv"
+today=`date +%Y-%m-%d`
+FILE=${temp}${today}${ext}
+
+echo $FILE
+
 DATAFOLDER="data"
 # Replace file_name with the target file's name to test.
 # Operator - The file test operator to use.
@@ -19,14 +25,13 @@ touch $FILE
 fi
 
 # Check if a data folder exists.
-if [[ ! -d $DATAFOLDER ]];
+if [[ ! -d $DATAFOLDER ]]
 then
-echo "$DATAFOLDER does not exist. Creating new directory"
+    echo "$DATAFOLDER does not exist. Creating new directory"
     mkdir $DATAFOLDER
-echo "MOVING $FILE into $DATAFOLDER"
+ else
+    echo "MOVING $FILE into $DATAFOLDER"
     mv $FILE $DATAFOLDER
-else
-rmdir $DATAFOLDER
 fi
 # If not, create it, then, move $FILE to it
 
